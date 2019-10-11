f1fc205eb71b34ad0c88e6f6d37021f82851dbe4
Ingólfur Orri <ingolfurorri@gmail.com>
2019-10-11 12:24:09 +0000

    a

--- airline_seating.py
+++ airline_seating.py
@@ -50,17 +50,15 @@ def is_taken(user_seat_list, seat_list):
         return True
     
     else:
-        print("Not taken")
         return False
 
 
+
 def update_seating(user_seat_list, seat_list):
+    '''Updates the seating chart'''
     user_row = int(user_seat_list[0])
     user_seat = SEATS.index(user_seat_list[1])
     seat_list[user_row-1][user_seat] = 'X'
-
-            
-
 
 
 
@@ -69,6 +67,10 @@ def get_user_seat(seat_list):
 
     while(is_taken(user_seat_list, seat_list)):
         print("That seat is taken!")
+        user_seat_list = input("Input seat number (row seat): ").split()
+
+    while not (is_valid(user_seat_list, seat_list)):
+        print("Seat number is invalid!")
         user_seat_list = input("Input seat number (row seat): ").split()
 
     else:
